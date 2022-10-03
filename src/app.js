eb833ee4848c4ec4f6eeae562b5429ae2d2c7586
RiseMary <113640605+RiseMary@users.noreply.github.com>
2022-10-03 21:05:43 +0300

    Update Current Location html and js

--- src/app.js
+++ src/app.js
@@ -69,6 +69,20 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getPosition() {
+  navigator.geolocation.getCurrentPosition(findPosition);
+}
+
+let locationButton = document.querySelector(".current-location");
+locationButton.addEventListener("click", getPosition);
+
+function findPosition(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
 function getForecast(coordinates) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
